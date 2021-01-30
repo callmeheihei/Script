e16bab08e2ccb77162838c8e4048a85a5e63fd56
hi_mmmjie <60500606+callmeheihei@users.noreply.github.com>
2021-01-30 14:41:53 +0800

    Update qyxj.js

--- Js/qyxj.js
+++ Js/qyxj.js
@@ -2,7 +2,7 @@
 QX:
 [rewrite_local]
 #轻颜相机（by黑黑酱）
-^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info url script-response-body qyxj.js
+^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info url script-response-body https://github.com/callmeheihei/QuantumultX/blob/master/Js/qyxj.js
 
 MITM:commerce-api.faceu.mobi
 
