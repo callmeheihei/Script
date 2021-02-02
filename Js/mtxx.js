ee09f1348e981d2d3df42e0bb1859426f4903f11
hi_mmmjie <60500606+callmeheihei@users.noreply.github.com>
2021-02-02 11:31:42 +0800

    Create mtxx.js

---
+++ Js/mtxx.js
@@ -0,0 +1,14 @@
+/*
+#美图秀秀会员
+QX:
+^https:\/\/api\.xiuxiu\.meitu\.com\/v1\/vip\/price\.json url 
+
+MITM = api.xiuxiu.meitu.com
+*/
+
+
+var obj = JSON.parse($response.body);
+obj.data.vip_type = 1;
+
+
+$done({body: JSON.stringify(obj)}); 
