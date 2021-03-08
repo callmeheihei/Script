5a50dd9540ac42e43d46ad03e386d126df5550a7
hi_mmmjie <60500606+callmeheihei@users.noreply.github.com>
2021-03-08 22:20:34 +0800

    Update mtxx.js

--- mtxx.js
+++ mtxx.js
@@ -1,22 +1,29 @@
-var body = $response.body;
-var url = $request.url;
+let obj = JSON.parse($response.body);
+obj = {
+{
+  "degrade": 0,
+  "ret": 0,
+  "error_code": 0,
+  "error": "Ok",
+  "msg": "成功",
+  "data": {
+    "id": 6764763235121907911,
+    "gid": 2100249674,
+    "uid": 1794626726,
+    "avatar_url": "http://maavatar1.meitudata.com/5fbde7a5c10621905.jpg",
+    "screen_name": "黑黑酱.",
+    "platform": 2,
+    "is_valid_user": 1,
+    "vip_type": 1,
+    "sub_type": 3,
+    "sub_name": "包年",
+    "renew": 2,
+    "valid_time": 1612845221,
+    "in_valid_time": 1613450021,
+    "create_time": 1612845238
+  }
+}
+  }]
+};
 
-const svip = '/v1/vip/vip_show.json';
-const  vip= '/v1/user/show.json';
-
-if (url.indexOf(svip) != -1) {
-    let obj = JSON.parse(body);
-    obj.data.is_valid_user = 1;
-    obj.data.vip_type = 1;
-    obj.data.sub_type = 3;
-	body = JSON.stringify(obj);  
- }
-
-if (url.indexOf(vip) != -1) {
-    let obj = JSON.parse(body);
-	obj.data.vip_type = 1;
-    obj.data.show_producer_level = 2;
-	body = JSON.stringify(obj); 
- }
-
-$done({body});
+$done({body: JSON.stringify(obj)});
