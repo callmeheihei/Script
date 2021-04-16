8e7e4306152e4d1f49f04925afae23b8fb30d329
hi_mmmjie <60500606+callmeheihei@users.noreply.github.com>
2021-04-16 11:13:17 +0800

    Update qyxj.js

--- Js/qyxj.js
+++ Js/qyxj.js
@@ -7,20 +7,10 @@ QX:
 MITM:commerce-api.faceu.mobi
 
 */
-
-const path1 = "/commerce/v1/subscription/user_info";
-
 let obj = JSON.parse($response.body);
-
-if ($request.url.indexOf(path1) != -1){
-obj.data.start_time = 1584674770;
-obj.data.end_time = 4077660370;
+obj.data.start_time = 1577813990;
+obj.data.end_time = 4102491933;
+obj.data.is_first_subscribe = true;
 obj.data.is_cancel_subscribe = true;
 obj.data.flag = true;
-}
-
-
-if ($request.url.indexOf(ad) != -1) {
-
-}
 $done({body: JSON.stringify(obj)});
